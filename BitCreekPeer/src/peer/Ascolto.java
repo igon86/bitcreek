b31382cdf7db2682056c65e8a92af0ea55b102db
alberto.bandettini <alberto.bandettini@0289b768-4b7e-11de-9ad6-1127a62b9fcd>
2009-05-28 20:48:37 +0000

    messo controllo per stesso file in download

--- BitCreekPeer/src/peer/Ascolto.java
+++ BitCreekPeer/src/peer/Ascolto.java
@@ -4,7 +4,9 @@ import condivisi.ErrorException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -37,20 +39,25 @@ public class Ascolto implements Runnable {
     public void run() {
         while (true) {
             try {
-                /**MANCA GESTIONE MONITOR + GESTIONE ERRORE + RISCRIVERE STE MILLE ECCEZZIONI 
-                 * + CONTATTARE L"ALTRO SE SONO LEECHER
-                 */
+
+                /* GESTIONE ERRORE + RISCRIVERE STE MILLE ECCEZZIONI */
+                
                 ObjectInputStream in = null;
                 ObjectOutputStream out = null;
                 Socket scambio = null;
                 peer.getIpServer().getHostAddress();
 
+                /* se non posso accettare connessioni vado in sleep come se fossi disconnesso */
+                if (peer.getConnessioni() >= BitCreekPeer.MAXCONNESSIONI) {
+                    throw new NullPointerException();
+                }
                 try {
                     scambio = peer.getSS().accept();
                 } catch (SocketTimeoutException e) {
                     // timeout scaduto : continuo a ciclare
-                    } catch (IOException ex) {
-                    System.err.println(Thread.currentThread().getName()+" Sono stato disconnesso : la socket di benvenuto è stata chiusa");
+                    continue;
+                } catch (IOException ex) {
+                    System.err.println(Thread.currentThread().getName() + " Sono stato disconnesso : la socket di benvenuto è stata chiusa");
                 }
                 try {
                     in = new ObjectInputStream(scambio.getInputStream());
@@ -65,41 +72,58 @@ public class Ascolto implements Runnable {
                     Creek contacted = peer.getCreek(swarmId);
                     //INVIO IL BITFILED RELATIVO
                     out.writeObject(new Bitfield(contacted.getHave()));
-                    /**
+                    /*
                      * EFFETTUO IL CONTROLLO CHE L'OGGETTO NON ESISTA GIA
                      */
                     Connessione conn = new Connessione(null, scambio, null, con.getSS());
                     Connessione temp;
                     if ((temp = contacted.presenzaConnessione(conn)) == null) {
-                        System.out.println(Thread.currentThread().getName()+" UPLOADER - SONO STATO CONTATTATO, connessione aggiunta");
+                        System.out.println(Thread.currentThread().getName() + " UPLOADER - SONO STATO CONTATTATO, connessione aggiunta");
                         contacted.addConnessione(conn);
-                    }
-                    else{
-                        System.out.println(Thread.currentThread().getName()+" UPLOADER - SONO STATO CONTATTATO, connessione gia presente");
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " UPLOADER - SONO STATO CONTATTATO, connessione gia presente");
+                        /* in teoria se esiste già una connessione in upload non dovrei fare niente
+                        Non c'è il controllo perchè per come è fatto sotto non ricontatto l'altro
+                        peer se ho già una connessione in down quindi vado avanti creando thread
+                        di upload ------------> controllare che sia così anche (e se) quando
+                        ci occuperemo dei download stoppati e ripartiti !!!*/
                         temp.setSocketUp(scambio);
                         conn = temp;
                     }
                     //CREO IL THREAD RELATIVO IN UPLOAD
                     peer.addTask(new Uploader(conn));
-                    //in.close();
-                    //out.close();
-                    //OPERAZIONI ULTERIORI SE SONO LEECHER nello SWARM
-                    //RICHIEDO ANCHE IO DI SCARICARE
-                    if (contacted.getStato() == true) {
-                        System.out.println(Thread.currentThread().getName()+" EFFETTUO OPERAZIONI AGGIUNTIVE PERCHE SONO LEECHER");
-                        Contact mycon = new Contact(peer.getMioIp(), peer.getPortaRichieste(), swarmId);
-                        Socket mysock = new Socket(con.getIp(), con.getSS());
-                        in = new ObjectInputStream(mysock.getInputStream());
-                        out = new ObjectOutputStream(mysock.getOutputStream());
-                        out.writeObject(mycon);
-                        Bitfield b = (Bitfield) in.readObject();
-                        conn.setSocketDown(mysock);
-                        conn.setBitfield(b.getBitfield());
-                        peer.addTask(new Downloader(contacted, conn));
+                    /* chiudo i file */
+                    in.close();
+                    out.close();
+                    /* incremento numero di connessioni */
+                    peer.incrConnessioni();
+                    /* operazioni ulteriori se sono leecher : creo connessione in down
+
+                    Lo devo fare solo se non ho già una connessione in down, non sono
+                    seeder e posso creare connessioni !!!! */
+                    if (contacted.getStato() == true/*&& !conn.downAttiva()*/ && peer.getConnessioni() < BitCreekPeer.MAXCONNESSIONI) {
+                        System.out.println("\n\nSONO ENTRATO\n\n");
+                            Contact mycon = new Contact(peer.getMioIp(), peer.getPortaRichieste(), swarmId);
+                            SocketAddress sa = new InetSocketAddress(con.getIp(), con.getSS());
+                            Socket mysock = new Socket();
+                            mysock.connect(sa, BitCreekPeer.TIMEOUTCONNESSIONE);
+                            in = new ObjectInputStream(mysock.getInputStream());
+                            out = new ObjectOutputStream(mysock.getOutputStream());
+                            out.writeObject(mycon);
+                            Bitfield b = (Bitfield) in.readObject();
+                            conn.setSocketDown(mysock);
+                            conn.setBitfield(b.getBitfield());
+                            System.out.println("Creo thread downloader perchè ho inviato mie credenzioali");
+                            /* aggiungo thread per download */
+                            peer.addTask(new Downloader(contacted, conn));
+                            /* incremento numero connessioni */
+                            peer.incrConnessioni();
                     }
                 } catch (IOException ex) {
+                    System.out.println("IOException in Ascolto");
                     Logger.getLogger(Ascolto.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (ClassNotFoundException ex) {
+                    System.out.println("ClassNotFoundException in Ascolto");
                     Logger.getLogger(Ascolto.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
@@ -109,7 +133,7 @@ public class Ascolto implements Runnable {
                 try {
                     Thread.sleep(ATTESA);
                 } catch (InterruptedException ex) {
-                    System.err.println(Thread.currentThread().getName()+" Interrotto thread Ascolto");
+                    System.err.println(Thread.currentThread().getName() + " Interrotto thread Ascolto");
                 }
             }
         }
