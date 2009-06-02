e579d35805176093b50ba19951c1c1bb78ffb846
andre.lotta86 <andre.lotta86@0289b768-4b7e-11de-9ad6-1127a62b9fcd>
2009-06-02 22:51:25 +0000

    Aggiunto compareTo per la lista PIO, ora e` ordinabile con le utils java -> e` possibile implemetare rarest!

--- trunk/BitCreekPeer/src/peer/PIO.java
+++ trunk/BitCreekPeer/src/peer/PIO.java
@@ -7,10 +7,10 @@ import java.io.Serializable;
  * Informazioni relative ad un chunk da scaricare
  * @author andrea
  */
-public class PIO implements Serializable{
+public class PIO implements Serializable, Comparable<PIO>{
     
     public static final long serialVersionUID = 45;
-
+    
     private int id;
     private int rarita;
     private boolean busy;
@@ -45,6 +45,26 @@ public class PIO implements Serializable{
     
     public void setBusy(){
         this.busy = true;
+    }
+    
+    /**
+     * il compare tiene conto di 2 fattori: il booleano busy e l'int rarita
+     * un PIO busy deve essere sicuramente posto dopo un PIO non busy
+     * @param arg0
+     * @return
+     */
+    public int compareTo(PIO arg0) {
+        int ret;
+        if(this.busy && ! arg0.busy){
+            ret = 1000+(this.rarita - arg0.rarita);
+        }
+        else if(! this.busy && arg0.busy){
+            ret = -1000+(this.rarita - arg0.rarita);;
+        }
+        else{
+            ret =(this.rarita - arg0.rarita);;
+        }
+        return ret;
     }
 }
 
