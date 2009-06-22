01cec039a38c6f9be5e4e73695671372201fa481
alberto.bandettini <alberto.bandettini@0289b768-4b7e-11de-9ad6-1127a62b9fcd>
2009-06-22 11:41:05 +0000

    modifica server come da specifica + controllo (diff) dei file in condivisi del server e client

--- trunk/BitCreekServer/src/condivisi/Descrittore.java
+++ trunk/BitCreekServer/src/condivisi/Descrittore.java
@@ -6,31 +6,30 @@ import java.io.Serializable;
  * Classe che definisce il descrittore di un file
  * @author Bandettini Lottarini
  */
-
-public class Descrittore implements Serializable{
+public class Descrittore implements Serializable {
 
     /* Costanti */
     public static final long serialVersionUID = 13;
     private final int NULL = -1;
-    
     /* Variabili d'istanza */
     private int id;
     /* campi riguardanti il file descritto */
     private String nomefile;
     private long dimensione;
-    private byte [] hash;
+    private byte[] hash;
     /* campi utili a chi usa il descrittore */
     private int portaTCP;
     private int portaUDP;
     private InterfacciaCallback stubcb;
     private int numSeeders;
     private int numLeechers;
-    
+
     /**
      * Costruttore vuoto
      */
-    
-    public Descrittore(){}
+    public Descrittore() {
+    }
+
     /**
      * Costruttore
      * @param nomefile nome del file
@@ -39,9 +38,8 @@ public class Descrittore implements Serializable{
      * @param stubcb interfaccia per callback
      * @throws condivisi.ErrorException se almeno un parametro non è valido
      */
-    
-    public Descrittore(String nomefile,long dimensione,byte [] hash,InterfacciaCallback stubcb) throws ErrorException{
-        if( nomefile == null || dimensione <= 0 || hash == null || stubcb == null){
+    public Descrittore(String nomefile, long dimensione, byte[] hash, InterfacciaCallback stubcb) throws ErrorException {
+        if (nomefile == null || dimensione <= 0 || hash == null || stubcb == null) {
             throw new ErrorException("Param null");
         }
         this.nomefile = nomefile;
@@ -51,43 +49,50 @@ public class Descrittore implements Serializable{
         this.portaUDP = NULL;
         this.stubcb = stubcb;
         /* il descrittore si crea solo a partire da un seeder*/
-        this.numSeeders=1;
-        this.numLeechers=0;
+        this.numSeeders = 1;
+        this.numLeechers = 0;
         this.id = -1;
     }
+
     /**metodo che ritorna il numero di Seeders attualmente sul descrittore*/
-    public int getNumSeeders(){
+    public synchronized int getNumSeeders() {
         return this.numSeeders;
     }
+
     /**metodo che ritorna il numero di leechers attualmente sul descrittore*/
-    public int getNumLeechers(){
+    public synchronized int getNumLeechers() {
         return this.numLeechers;
     }
-    public synchronized void setNumSeeders(int num){
+
+    public synchronized void setNumSeeders(int num) {
         this.numSeeders = num;
     }
-    public synchronized void setNumLeechers(int num){
+
+    public synchronized void setNumLeechers(int num) {
         this.numLeechers = num;
     }
+
     /**
      * Restituisce il nome del file presente nel descrittore
      * @return nomefile
      */
-    public String getName(){
+    public String getName() {
         return this.nomefile;
     }
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
-    public void setId(int id){
+
+    public void setId(int id) {
         this.id = id;
     }
+
     /**
      * Restituisce la dimensione del file presente nel descrittore
      * @return dimensione
      */
-    public long getDimensione(){
+    public long getDimensione() {
         return this.dimensione;
     }
 
@@ -95,7 +100,7 @@ public class Descrittore implements Serializable{
      * restituisce la stringa hash del file presente nel descrittore
      * @return hash
      */
-    public byte [] getHash(){
+    public byte[] getHash() {
         return this.hash;
     }
 
@@ -103,7 +108,7 @@ public class Descrittore implements Serializable{
      * Restituisce il numero della porta del tracker TCP
      * @return portaTCP, -1 se non impostata
      */
-    public int getTCP(){
+    public int getTCP() {
         return this.portaTCP;
     }
 
@@ -111,7 +116,7 @@ public class Descrittore implements Serializable{
      * Restituisce il numero della porta del tracker UDP
      * @return portaUDP, -1 se non impostata
      */
-    public int getUDP(){
+    public int getUDP() {
         return this.portaUDP;
     }
 
@@ -119,7 +124,7 @@ public class Descrittore implements Serializable{
      * Restituisce l'interfaccia per la callback
      * @return stubcb
      */
-    public InterfacciaCallback getCallback(){
+    public InterfacciaCallback getCallback() {
         return this.stubcb;
     }
 
@@ -127,13 +132,14 @@ public class Descrittore implements Serializable{
      * Setta la porta TCP a porta
      * @param porta
      */
-    public void setPortaTCP(int porta){
-        if(porta <= 0)
+    public void setPortaTCP(int porta) {
+        if (porta <= 0) {
             porta = NULL;
+        }
         this.portaTCP = porta;
     }
 
-    public Descrittore copia() throws ErrorException{
+    public Descrittore copia() throws ErrorException {
         Descrittore d = null;
         try {
             //System.out.println("COPIA - DESCRITTORE");
@@ -145,6 +151,7 @@ public class Descrittore implements Serializable{
         d.portaUDP = portaUDP;
         d.numLeechers = numLeechers;
         d.numSeeders = numSeeders;
+        d.id = id;
         return d;
     }
 
@@ -152,9 +159,10 @@ public class Descrittore implements Serializable{
      * Setta la porta UDP a porta
      * @param porta
      */
-    public void setPortaUDP(int porta){
-        if(porta <= 0)
+    public void setPortaUDP(int porta) {
+        if (porta <= 0) {
             porta = NULL;
+        }
         this.portaUDP = porta;
     }
 
@@ -163,7 +171,7 @@ public class Descrittore implements Serializable{
      * @return nomefile.hahsCode
      */
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return this.nomefile.hashCode();
     }
 
