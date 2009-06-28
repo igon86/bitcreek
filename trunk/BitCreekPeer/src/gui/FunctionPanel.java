0cef23a5d34ded197f255a8df8395e73475974d7
alberto.bandettini <alberto.bandettini@0289b768-4b7e-11de-9ad6-1127a62b9fcd>
2009-06-28 15:05:08 +0000

    ...classe del grafico commentata...

--- trunk/BitCreekPeer/src/gui/FunctionPanel.java
+++ trunk/BitCreekPeer/src/gui/FunctionPanel.java
@@ -5,34 +5,59 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
+/**
+ * Clase che crea e aggiorna il grafico
+ * @author Bandettini Alberto
+ * @author Lottarini Andrea
+ * @version BitcreekPeer 1.0
+ */
 class FunctionPanel extends JPanel {
 
+    /* Costanti */
+    /** Costante che definisce la versione della classe */
     public static final long serialVersionUID = 100;
+    /** Definisce l' altezza del grafico */
     private final int ALTEZZA = 130;
+    /** Definisce il punto iniziale in pixel, asse x */
     private final int INIZIOX = 30;
+    /** Definisce il punto iniziale in pixel, asse y */
     private final int INIZIOY = 25;
+    /** Definisce lo spazio in pixel tra una riga del grafico e la successiva */
     private final int SPAZIO = 20;
 
     /* Variabili d' istanza */
+    /** Larghezza del grafico in pixel */
     private int larghezza;
     /* Variabili di classe */
+    /** Array di punti da disegnare */
     private static ArrayList<Integer> array = new ArrayList<Integer>();
 
+    /**
+     * Costruttore del grafico
+     */
     public FunctionPanel() {
         super();
         this.larghezza = 250;
     }
 
+    /**
+     * Aggiunge un nuovo punto all' array
+     * @param c
+     */
     public static void settaConnessioni(int c) {
         if (c < 0) {
             c = 0;
         }
-        if (c > /*100*/10) {
-            c = /*100*/10;
+        if (c > /*100*/ 10) {
+            c = /*100*/ 10;
         }
         array.add(0, new Integer(c));
     }
 
+    /**
+     * Disegna il grafico
+     * @param g Graphics su cui disegnare
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -49,7 +74,7 @@ class FunctionPanel extends JPanel {
         }
         int n = 0, j = 0;
         for (--i; i >= 0; i--) {
-            n = (i * /*SPAZIO*/2);
+            n = (i * /*SPAZIO*/ 2);
             g.drawString("" + n, INIZIOX - 25, INIZIOY + j++ * SPAZIO + 5);
         }
         g.drawString("Tempo", INIZIOX + (larghezza / 2) - 40, ALTEZZA + 16);
@@ -67,15 +92,15 @@ class FunctionPanel extends JPanel {
         g.setColor(Color.blue);
         if (array.size() > 0) {
             connessioni = array.get(0).intValue();
-            g.drawLine(INIZIOX, ALTEZZA - 5 - 10*connessioni, INIZIOX, ALTEZZA - 5 - 10*connessioni);
+            g.drawLine(INIZIOX, ALTEZZA - 5 - 10 * connessioni, INIZIOX, ALTEZZA - 5 - 10 * connessioni);
             int ultimox = INIZIOX;
-            int ultimoy = ALTEZZA - 5 - 10*connessioni;
+            int ultimoy = ALTEZZA - 5 - 10 * connessioni;
             int nuovox = ultimox + 10;
             for (int i = 1; i < array.size(); i++) {
                 connessioni = array.get(i).intValue();
-                g.drawLine(ultimox, ultimoy, nuovox, ALTEZZA - 5 - 10*connessioni);
+                g.drawLine(ultimox, ultimoy, nuovox, ALTEZZA - 5 - 10 * connessioni);
                 ultimox = nuovox;
-                ultimoy = ALTEZZA - 5 - 10*connessioni;
+                ultimoy = ALTEZZA - 5 - 10 * connessioni;
                 nuovox += 10;
                 if (nuovox > larghezza - 13) {
                     break;
@@ -84,4 +109,3 @@ class FunctionPanel extends JPanel {
         }
     }
 }
-
